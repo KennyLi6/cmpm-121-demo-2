3f554fd52dc8ee99f9d786891b903571f8ea1a71
KennyLi6 <kennyli3219@gmail.com>
2024-10-13 19:22:56 -0700

    making sure deno is set up properly

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Ensuring Deno is working as expected";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
