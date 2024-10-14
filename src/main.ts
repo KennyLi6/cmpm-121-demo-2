d59711af0b83228ae8ff86c5e63a4e71a6ac6aaa
KennyLi6 <kennyli3219@gmail.com>
2024-10-13 20:04:05 -0700

    added a blue canvas element - step 1

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
 import "./style.css";
 
-const APP_NAME = "Ensuring Deno is working as expected";
+const APP_NAME = "Drawer";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const header = document.createElement("h1");
+header.innerHTML = APP_NAME;
+app.append(header);
+
+const canvasSize = 256;
+const canvas = document.createElement("canvas");
+canvas.width = canvas.height = canvasSize;
+app.append(canvas);
+
+const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+if (!context) { //check if context of canvas actually exists
+    console.error("Context not found!");
+}
+
+context.fillStyle = '#34BAEB';
+context.fillRect(0, 0, canvasSize, canvasSize);
